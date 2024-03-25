243576570c1d4febeab040e3418640b091383a47
HarshithS411 <159102941+HarshithS411@users.noreply.github.com>
2024-03-25 12:59:12 +0530

    JS 25/03/24

---
+++ JS/index.js
@@ -0,0 +1,59 @@
+console.log("Hello World");
+console.log(1 + 2);
+
+const PI = 3.1415;
+console.log("PI value is");
+
+var x = 1;
+console.log(x);
+var y = 1.4;
+console.log(y);
+var z = "Hello";
+console.log(z);
+var a = [x, y, z];
+console.log(a);
+var lst = [1, "0", [1,2], 4.8, true];
+console.log(lst);
+var obj = {
+    name : "Mr X",
+    job : "Hacker",
+    age : 32,
+    address : "Bengaluru"
+}
+console.log(obj);
+
+let phone = {
+    company: {
+        name : "Apple",
+        model : "14pro"
+    },
+    name : 'Galaxy',
+    size : 6.5,
+    weight : 200,
+    color : ['blue', 'red', 'green']
+}
+
+let str1 = 'Hello';
+let str2 = "Hello";
+let str3 = `Hello World`;
+console.log(str3);
+
+let outcome = [
+                [1, 2, 3, 4, 5, 6], 
+                [7, 8, 9, 10, 11, 12], 
+                [13, 14, 15, 16, 17,18]
+            ]
+console.log(outcome[1][3]);
+console.log(outcome[2][5]);
+
+let d3 = [
+    [
+        [1,2], [2, 1]
+    ],
+    [
+        [3, 4], [4, 3]
+    ],
+    [
+        [5, 6], [6,5]
+    ]
+]
